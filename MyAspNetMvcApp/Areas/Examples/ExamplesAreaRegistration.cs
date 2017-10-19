483bbe09fdab288745e0040f2ec9987827f0f683
Bernard Gabon <hewbertgabon@gmail.com>
2017-10-19 13:06:01 +0800

    Added Notification navbar

---
+++ MyAspNetMvcApp/Areas/Examples/ExamplesAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MyAspNetMvcApp.Areas.Examples
+{
+    public class ExamplesAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Examples";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Examples_default",
+                "Examples/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
